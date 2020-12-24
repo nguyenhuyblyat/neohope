21f2b7f3dc7c5832902e5ff3aa50a1a944b812c9
Nguyen Gia Huy <minhaloli77@gmail.com>
2020-12-24 21:33:04 +0700

    remove certain things and added stuff

--- README.md
+++ README.md
@@ -1,15 +1,24 @@
 # neohope
   A shitty bot I made to cyberbully you.
 
+
+  20/4/2014
+  please help me i am trapped under my uncle's basement. hope he doesn't find out.
+  my coordinates are 21.04976031675703,105.82223221956679 please send help
 # Author:
   - @nguyenhuyblyat#3863
 
 # Uses:
-  - tell joke
-  - insult
+  - tell dad jokes
+  - insults you
   - economy [under development]
-  - play music/recordings [under development]
+  - play music/recordings of me [under development]
   - profile info aka. web scraping [only specific websites]
+
+# Commands:
+  - $help - Link to this GitHub repository.
+  - $insult - insult you haha dumbfuck
+  - $joke - random dad jokes
 
 # Languages:
   - Javascript
--- app.js
+++ app.js
@@ -14,6 +14,7 @@ client.once('ready', () => {
 
 p = process.env.prefix;
 
+insults =['Keep talking, someday you will say something intelligent.', 'YOUR LIFE IS SO PATHETIC I RAN A 15K TO RAISE AWARENESS FOR IT', 'The smartest thing thats ever come from your mouth was my dick']
 // reply != send !!!
 // reply = reply and mention, send = send a message
 client.on('message', (msg) => {
@@ -22,6 +23,8 @@ client.on('message', (msg) => {
     msg.channel.send(`lmao ok boi`);
   } else if (msg.content.startsWith(`${p}help`)) {
     msg.channel.send(`List of commands: https://github.com/nguyenhuyblyat/blob/main/README.md`);
+  } else if (msg.content.startsWith(`${p}insult`)) {
+      msg.channel.send(insults[(Math.floor(Math.random() * insults.length))]);
   }
 });
 
