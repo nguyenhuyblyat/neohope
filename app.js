b76f446f6baa979734dba1ff2dec079a03c0a4da
Nguyen Gia Huy <minhaloli77@gmail.com>
2020-12-24 16:55:10 +0700

    i think i fixed a bug...

--- app.js
+++ app.js
@@ -9,19 +9,20 @@ require('dotenv').config();
 const client = new Discord.Client();
 
 client.once('ready', () => {
-  console.log("NeoHope is now up!")
+  console.log("NeoHope is now up!");
 });
 
+p = process.env.prefix;
 
 // reply != send !!!
 // reply = reply and mention, send = send a message
-
 client.on('message', (msg) => {
-  console.log(msg)
-  if (msg.content === 'test echo') {
-    msg.channel.reply("Exit code: 0; test OK.")
+  // debug echo test
+  if (msg.content.startsWith(`${p}ping`)) {
+    msg.channel.send(`lmao ok boi`);
+  } else if (msg.content.startsWith(`${p}help`)) {
+    msg.channel.send(`List of commands: https://github.com/nguyenhuyblyat/blob/main/README.md`);
   }
 });
-
 
 client.login(process.env.bot_token);
